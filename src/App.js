e34ebcbed51dadd6926f581bee78cd6dd1b6b928
overlord-17 <deogharia.17@gmail.com>
2021-03-03 23:59:29 +0530

    fixing some errors

--- src/App.js
+++ src/App.js
@@ -80,22 +80,22 @@ class App extends Component {
         })
       })
       .then(response => response.json())
-      .then(response => {
-        if (response) {
-          fetch('https://stormy-lowlands-09387.herokuapp.com/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            // .then(response => response.json())
-            // .then(count => {
-            //   this.setState(Object.assign(this.state.user, { entries: count}))
-            // })
-            // .catch(console.log)
+      // .then(response => {
+      //   if (response) {
+      //     fetch('https://stormy-lowlands-09387.herokuapp.com/image', {
+      //       method: 'put',
+      //       headers: {'Content-Type': 'application/json'},
+      //       body: JSON.stringify({
+      //         id: this.state.user.id
+      //       })
+      //     })
+      //       // .then(response => response.json())
+      //       // .then(count => {
+      //       //   this.setState(Object.assign(this.state.user, { entries: count}))
+      //       // })
+      //       // .catch(console.log)
 
-        }
+      //   }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
       .catch(err => console.log(err));
